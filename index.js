c71d2f908d3514db83a928164e6d3b50e590463f
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-11-10 14:39:02 +0600

    delete api to delete my export products added

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/products/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = { _id: new ObjectId(id)}
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // Imported Products APIs
 
         app.get('/imports', async (req, res) => {
